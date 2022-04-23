a826ef016b0cb7f4ae7347345df3c38cc5282634
ajordan10 <abby.jordan@ou.edu>
2022-04-23 17:48:50 -0500

    Initialize2

---
+++ R/Jordan_Abby_Package3.R
@@ -0,0 +1,36 @@
+library(kableExtra)
+#' PacakageProject3
+#'
+#' @param x X variable as as vector
+#' @param y Y variable as a vector
+#' @param alpha
+#'
+#' @return A constructor
+#' @export
+#'
+#' @examples
+#' Insert Example
+myconstr = function(x, y, alpha){
+  Rttest2 <- t.test(x, y, mu = 0, var.equal = TRUE, conf.level = 1-alpha)
+  Rttest <- list(data = data.frame(x=x, y=y), Confidence_Interval = Rttest2$conf.int, P.value = Rttest2$p.value, Alpha = alpha)
+  class(Rttest)
+  Rttest
+}
+#Print Function
+print.Rttest <- function(x,...) {
+  data = c(Rttest$data,Rttest$Confidence_Interval,Rttest$P.value,Rttest$Alpha)
+  kable(Rttest$data)
+
+}
+#data
+set.seed(21)
+x <- rnorm(30,5,2)
+
+set.seed(23)
+y <- rnorm(30,3,2)
+
+alpha <- 0.05
+
+Rttest = myconstr(x, y, alpha)
+class(Rttest)
+kableExtra::kable(print(Rttest))
